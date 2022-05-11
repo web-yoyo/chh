19933be6cace791104818b8e01babaa5f7554181
CHH <aabbbbbcc@163.com>
2022-05-11 18:03:15 +0800

    ✨ feat:
    按需引入vant ui

--- src/components/my-hello.vue
+++ src/components/my-hello.vue
@@ -1,5 +1,6 @@
 <template>
   <h1>test</h1>
+  <van-button type="success">成功按钮</van-button>
 </template>
 
 <script setup lang="ts"></script>
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import { Button } from 'vant';
 
-createApp(App).mount('#app')
+const app = createApp(App);
+app.use(Button);
+
+app.mount('#app')
