eb80f01e411daab74fd8a9d885c10e2d7d984390
aabbbbbcc <aabbbbbcc@163.com>
2022-06-10 23:59:38 +0800

    修复初始值

--- src/views/Test/index.vue
+++ src/views/Test/index.vue
@@ -25,19 +25,17 @@
 </template>
 
 <script setup lang="ts">
-  import { ref, watch, onMounted, nextTick } from 'vue'
+  import { ref, watch, nextTick } from 'vue'
   let value = ref('')
   let oldStr = 'aaaahffghyhy哈哈回复健康急急急地方快递费浏览经济酷酷酷'
   let str = ref('aaaahffghyhy哈哈回复健康急急急地方快递费浏览经济酷酷酷')
-  onMounted(() => {
-    // const dom = document.querySelector('.text') as HTMLImageElement
-    // dom.innerHTML = str.value
-  })
+
   watch(value, (val) => {
     if (!val) {
       str.value = oldStr
       return
     }
+    str.value = oldStr
     str.value = str.value.replaceAll(val, `<span style="color:red">${val}</span>`)
     console.log(str.value)
     const dom = document.querySelector('.text') as HTMLImageElement
