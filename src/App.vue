6bcdf845ff9b457821c7f535df567927e39b8ae2
web-yoyo <11035288@qq.com>
2022-07-04 09:49:45 +0800

    ✨ feat: 判断版本
    微信

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,19 @@
   <router-view />
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+  const checkWeixin = () => {
+    var u = navigator.userAgent
+    console.log(u, 'navigator.userAgent')
+
+    // 判断是否为微信浏览器
+    if (u.indexOf('MicroMessenger') > -1 || u.indexOf('MQQBrowser') > -1) {
+      return true
+    }
+    return false
+  }
+  console.log(checkWeixin())
+</script>
 
 <style>
   #app {
